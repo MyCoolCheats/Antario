ee490a849c067788ce45bd138053ef4c2d6842ab
certmemer <verynice1@memeware.net>
2017-10-18 15:01:23 +0200

    griefing is bullying

--- Castle/AutoBlock.cpp
+++ Castle/AutoBlock.cpp
@@ -7,48 +7,7 @@ void Autoblock::CreateMove(CUserCmd* cmd)
 {
 	if (!Settings::Autoblock::enabled)
 		return;
-
-	if (!pInputSystem->IsButtonDown(Settings::Autoblock::key))
+	
+	if (Settings::Autoblock::enabled)
 		return;
-
-	C_BasePlayer* localplayer = (C_BasePlayer*)pEntityList->GetClientEntity(pEngine->GetLocalPlayer());
-	float bestdist = 250.f;
-	int index = -1;
-
-	for (int i = 1; i < pEngine->GetMaxClients(); i++)
-	{
-		C_BasePlayer* entity = (C_BasePlayer*)pEntityList->GetClientEntity(i);
-
-		if (!entity)
-			continue;
-
-		if (!entity->GetAlive() || entity->GetDormant() || entity == localplayer)
-			continue;
-
-		float dist = localplayer->GetVecOrigin().DistTo(entity->GetVecOrigin());
-
-		if (dist < bestdist)
-		{
-			bestdist = dist;
-			index = i;
-		}
-	}
-
-	if (index == -1)
-		return;
-
-	C_BasePlayer* target = (C_BasePlayer*)pEntityList->GetClientEntity(index);
-
-	if (!target)
-		return;
-
-	Vector angles = Math::CalcAngle(localplayer->GetVecOrigin(), target->GetVecOrigin());
-
-	angles.y -= localplayer->GetEyeAngles()->y;
-	Math::NormalizeAngles(angles);
-
-	if (angles.y < 0.0f)
-		cmd->sidemove = 450.f;
-	else if (angles.y > 0.0f)
-		cmd->sidemove = -450.f;
 }
